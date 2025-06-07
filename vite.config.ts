a124ad10a9b6dd570557fb0fec776a031eea67fa
Pedro Lucas Dos Santos <90657983+PLSR12@users.noreply.github.com>
2025-06-07 17:04:32 -0300

    add base to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -6,37 +6,9 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/",
   resolve: {
-    alias: [
-      {
-        find: "@",
-        replacement: fileURLToPath(new URL("./src", import.meta.url)),
-      },
-      {
-        find: "@assets",
-        replacement: fileURLToPath(new URL("./src/assets", import.meta.url)),
-      },
-      {
-        find: "@components",
-        replacement: fileURLToPath(new URL("./src/components", import.meta.url)),
-      },
-      {
-        find: "@layouts",
-        replacement: fileURLToPath(new URL("./src/layouts", import.meta.url)),
-      },
-      {
-        find: "@pages",
-        replacement: fileURLToPath(new URL("./src/pages", import.meta.url)),
-      },
-      {
-        find: "@styles",
-        replacement: fileURLToPath(new URL("./src/styles", import.meta.url)),
-      },
-      {
-        find: "@utils",
-        replacement: fileURLToPath(new URL("./src/utils", import.meta.url)),
-      },
-    ],
+    alias: [],
   },
   build: {
     outDir: path.join(__dirname, "build"),
